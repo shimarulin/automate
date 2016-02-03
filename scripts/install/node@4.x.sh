b5145f3ff945c2cba01cccfcd3accbb528734be2
Vyacheslav Shimarulin <shimarulin@gmail.com>
2016-02-03 06:35:04 +0300

    upd node.js install script

--- scripts/install/node@4.x.sh
+++ scripts/install/node@4.x.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+apt-get install curl
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get install -y nodejs build-essential
+apt-get install -y nodejs build-essential
