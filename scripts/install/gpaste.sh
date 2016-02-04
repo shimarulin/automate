ac18ea276bd5842429ac09afa12991eb7e62678e
Vyacheslav Shimarulin <shimarulin@gmail.com>
2016-02-04 11:50:35 +0300

    gpaste install script update

--- scripts/install/gpaste.sh
+++ scripts/install/gpaste.sh
@@ -7,7 +7,7 @@ unzip -d /tmp $ZIP_FILE
 
 pushd /tmp/GPaste-$VERSION
 
-sudo apt-get -y install libtool valac gobject-introspection intltool libappstream-glib-dev gnome-control-center-dev libgirepository1.0-dev libclutter-1.0-dev
+sudo apt-get -y install libtool valac gobject-introspection intltool libappstream-glib-dev gnome-control-center-dev libgirepository1.0-dev libclutter-1.0-dev appstream-util
 ./autogen.sh && ./configure --sysconfdir=/etc && make && sudo make install && sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
 
 popd
